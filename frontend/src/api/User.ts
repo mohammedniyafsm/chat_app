a143fe49433a3fe612a6499046ac770d03b0b4da
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-07-25 22:27:24 +0530

    added .gitignore and env backend ur

--- frontend/src/api/User.ts
+++ frontend/src/api/User.ts
@@ -1,78 +1,75 @@
-import axios from "axios"
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+import axios from "axios";
 
 interface SignupType {
-    email : string,
-    username: string,
-    password : string
+  email: string;
+  username: string;
+  password: string;
 }
+
 interface SigninType {
-    email : string,
-    password : string
+  email: string;
+  password: string;
 }
 
 interface sendMessageType {
-    token : string,
-    receiverId : string,
-    message : string
+  token: string;
+  receiverId: string;
+  message: string;
 }
 
+export const SignupUser = async (data: SignupType) => {
+  const response = await axios.post(`${BASE_URL}/user/signup`, data);
+  return response;
+};
 
-export const SignupUser = async ( data :SignupType)=>{
-    const response = await axios.post('http://localhost:3001/user/signup',data)
-    return response;
-}
-export const SigninUser = async ( data :SigninType)=>{
-    const response = await axios.post('http://localhost:3001/user/signin',data)
-    return response;
-}
+export const SigninUser = async (data: SigninType) => {
+  const response = await axios.post(`${BASE_URL}/user/signin`, data);
+  return response;
+};
 
-export const AllUserList = async(token : string,query?:string)=>{
-    if(query){
-        const response = await axios.get(`http://localhost:3001/user/users?search=${query}`,{
-            headers :{
-                Authorization : `Bearer ${token}`
-            }
-        })
-        console.log(response.data);
-        
-        return response.data
+export const AllUserList = async (token: string, query?: string) => {
+  const url = query
+    ? `${BASE_URL}/user/users?search=${query}`
+    : `${BASE_URL}/user/users`;
+
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+};
+
+export const sendMessage = async ({ receiverId, message, token }: sendMessageType) => {
+  const response = await axios.post(
+    `${BASE_URL}/user/send`,
+    { receiverId, message },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     }
-    else{
-        const response = await axios.get('http://localhost:3001/user/users',{
-            headers :{
-                Authorization : `Bearer ${token}`
-            }
-        })
-        return response.data
-    }
-}
+  );
+  return response;
+};
 
-export const sendMessage= async({ receiverId,message,token }:sendMessageType)=>{
-    const response = await axios.post('http://localhost:3001/user/send',{
-        receiverId,
-        message
-    },{
-        headers : {
-            Authorization : `Bearer ${token}`
-        }
-    })
-    return response
-}
- 
-export const getMessage = async( token : string,query:string )=>{
-    const response = await axios.get(`http://localhost:3001/user/getmessage?receiverId=${query}`,{
-        headers : {
-            Authorization : `Bearer ${token}`
-        }
-    })
-    return response.data;
-}
+export const getMessage = async (token: string, query: string) => {
+  const response = await axios.get(`${BASE_URL}/user/getmessage?receiverId=${query}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
 
-export const getUserDetails = async(token:string)=>{
-    const response = await axios.get(`http://localhost:3001/user/mydata`,{
-        headers :{
-            Authorization : `Bearer ${token}`
-        }
-    })
-    return response.data;
-}
+export const getUserDetails = async (token: string) => {
+  const response = await axios.get(`${BASE_URL}/user/mydata`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
