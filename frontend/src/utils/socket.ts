934f197f3ddd9febf897f3685608ca82dc852f45
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-07-25 23:08:06 +0530

    added server url

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,9 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
+const socket = io(import.meta.env.VITE_API_BASE_URL, {
   withCredentials: true,
   transports: ['websocket'],
 });
 
 export default socket;
-
