d0a21e9346ae65c41fda6c9cf8c475f1da289a5e
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-07-25 22:11:19 +0530

    Remove node_modules and .env from tracking

--- backend/dist/utils/chat.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRoomId = getRoomId;
-function getRoomId(userId1, userId2) {
-    return [userId1, userId2].sort().join("_");
-}
